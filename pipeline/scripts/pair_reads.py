f768527955d3fc27b4b3ec1353004012fd15a895
Nicholas Mateyko <nick.mateyko@gmail.com>
2022-12-10 17:25:48 -0800

    Convert all to 4 spaces

--- pipeline/scripts/pair_reads.py
+++ pipeline/scripts/pair_reads.py
@@ -6,40 +6,40 @@
 from myutils import readFastq
 
 def revcomp(seq):
-	if not set(seq).issubset({'A', 'C', 'G', 'T', 'N'}):
-		raise ValueError(f"Sequence {seq} must only contain ACTGN")
-	comp = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
-	return "".join([comp[i] for i in seq[::-1]])
+    if not set(seq).issubset({'A', 'C', 'G', 'T', 'N'}):
+        raise ValueError(f"Sequence {seq} must only contain ACTGN")
+    comp = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    return "".join([comp[i] for i in seq[::-1]])
 
 
 def testAlignment(seq1, seq2, offset):
-	score = 0.0
-	for i in range(offset, len(seq1)):
-		if seq1[i] == seq2[i-offset]:
-			score += 1
-		#else:
-		#	print("mismatch at %i"%(i));
-	return score/(len(seq1)-offset) 
+    score = 0.0
+    for i in range(offset, len(seq1)):
+        if seq1[i] == seq2[i-offset]:
+            score += 1
+        #else:
+        #	print("mismatch at %i"%(i));
+    return score/(len(seq1)-offset) 
 
 
 def align(seq1, seq2, offset, overlapRange):
-	for i in range(0,overlapRange+1):
-		if testAlignment(seq1,seq2, offset + i) > 0.75:
-			return (offset + i)
-		elif testAlignment(seq1,seq2, offset - i) > 0.75:
-			return (offset - i)
+    for i in range(0,overlapRange+1):
+        if testAlignment(seq1,seq2, offset + i) > 0.75:
+            return (offset + i)
+        elif testAlignment(seq1,seq2, offset - i) > 0.75:
+            return (offset - i)
 
 
 def getConsensus(read1, read2, offset):
-	consensus  = []
-	#print("from %i to %i"%(offset,len(read1[1])))
-	for i in range(offset, len(read1[1])):
-		if read1[2][i] > read2[2][i-offset]: #converts to ASCII, corresponds to quality; if equal, doesn't matter which is better since base is the same
-			consensus.append(read1[1][i])
-		else:
-			consensus.append(read2[1][i-offset])
-	#print("read 2 from %i"%((len(read1[1])-offset)))
-	return read1[1][0:(offset)] + "".join(consensus) + read2[1][(len(read1[1])-offset):]
+    consensus  = []
+    #print("from %i to %i"%(offset,len(read1[1])))
+    for i in range(offset, len(read1[1])):
+        if read1[2][i] > read2[2][i-offset]: #converts to ASCII, corresponds to quality; if equal, doesn't matter which is better since base is the same
+            consensus.append(read1[1][i])
+        else:
+            consensus.append(read2[1][i-offset])
+    #print("read 2 from %i"%((len(read1[1])-offset)))
+    return read1[1][0:(offset)] + "".join(consensus) + read2[1][(len(read1[1])-offset):]
 
 
 r1_fastq_fp = snakemake.input[0]
@@ -48,20 +48,20 @@ out_fp = snakemake.output[0]
 log_fp = snakemake.log[0]
 
 with open(r1_fastq_fp, 'rt') as r1, \
-	   open(r2_fastq_fp, 'rt') as r2, \
-		 open(out_fp, 'wt') as out_file, \
-		 open(log_fp, 'wt') as log_file:
+       open(r2_fastq_fp, 'rt') as r2, \
+         open(out_fp, 'wt') as out_file, \
+         open(log_fp, 'wt') as log_file:
 
-	r1_reader = readFastq(r1)
-	r2_reader = readFastq(r2)
-	
-	for r1_read, r2_read in zip(r1_reader, r2_reader):
-		r1_seq = r1_read[1]
-		r2_seq = r2_read[1]
+    r1_reader = readFastq(r1)
+    r2_reader = readFastq(r2)
+    
+    for r1_read, r2_read in zip(r1_reader, r2_reader):
+        r1_seq = r1_read[1]
+        r2_seq = r2_read[1]
 
-		r2_read = (r2_read[0], revcomp(r2_read[1]), r2_read[2][::-1])
-		if testAlignment(r1_read[1], r2_read[1], 0) > 0.8:
-			consensus = getConsensus(r1_read, r2_read, 0)
-			out_file.write(f"{consensus}\n")
-		else:
-			log_file.write(f"Skipping {r1_read[0]}/{r2_read[0]} because they don't align within given parameters\nr1: {r1_seq}\nr2: {r2_seq}\n")
+        r2_read = (r2_read[0], revcomp(r2_read[1]), r2_read[2][::-1])
+        if testAlignment(r1_read[1], r2_read[1], 0) > 0.8:
+            consensus = getConsensus(r1_read, r2_read, 0)
+            out_file.write(f"{consensus}\n")
+        else:
+            log_file.write(f"Skipping {r1_read[0]}/{r2_read[0]} because they don't align within given parameters\nr1: {r1_seq}\nr2: {r2_seq}\n")
--- pipeline/scripts/starcode_to_counter.py
+++ pipeline/scripts/starcode_to_counter.py
@@ -4,23 +4,23 @@ from collections import Counter
 from myutils import readFastq
 
 def starcode_clusters_to_counter(clustered_path, unclustered_path):
-  with open(unclustered_path, 'r') as f:
-    reads = [line.strip() for line in f]
-  with open(clustered_path, 'r') as f:
-    clustered_lines = [tuple(line) for line in csv.reader(f, delimiter='\t')]
+    with open(unclustered_path, 'r') as f:
+        reads = [line.strip() for line in f]
+    with open(clustered_path, 'r') as f:
+        clustered_lines = [tuple(line) for line in csv.reader(f, delimiter='\t')]
 
-  cluster_counts = {}
+    cluster_counts = {}
 
-  for cluster in clustered_lines:
-    indices = [int(i) for i in cluster[3].split(',')]
-    cluster_seqs = [reads[i - 1] for i in indices] # Sequence IDs start at 1
-    cluster_counts[cluster[0]] = Counter(cluster_seqs)
+    for cluster in clustered_lines:
+        indices = [int(i) for i in cluster[3].split(',')]
+        cluster_seqs = [reads[i - 1] for i in indices] # Sequence IDs start at 1
+        cluster_counts[cluster[0]] = Counter(cluster_seqs)
 
-  return cluster_counts
+    return cluster_counts
 
 fastq_path = snakemake.input[0]
 cluster_path = snakemake.input[1]
 cluster_counter = starcode_clusters_to_counter(cluster_path, fastq_path)
 
 with open(snakemake.output[0], 'wb') as f:
-  pickle.dump(cluster_counter, f, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(cluster_counter, f, protocol=pickle.HIGHEST_PROTOCOL)
